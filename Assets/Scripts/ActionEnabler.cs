ec4f11e0a7172493ab51598cb439f3300c527ea4
John <jdschae@umich.edu>
2017-10-16 07:41:28 -0400

    added menu options

---
+++ Assets/Scripts/ActionEnabler.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HoloToolkit.Unity.InputModule;
+
+public class ActionEnabler : MonoBehaviour {
+
+
+    public void EnableMarkerPlacement(GameObject Model)
+    {
+        Model.GetComponent<MarkerPlace>().IsPlacementEnabled = true;
+        Model.GetComponent<HandDraggable>().IsDraggingEnabled = false;
+        Model.GetComponent<HandRotate>().IsRotatingEnabled = false;
+    }
+
+    public void EnableDragging(GameObject Model)
+    {
+        Model.GetComponent<MarkerPlace>().IsPlacementEnabled = false;
+        Model.GetComponent<HandDraggable>().IsDraggingEnabled = true;
+        Model.GetComponent<HandRotate>().IsRotatingEnabled = false;
+    }
+
+    public void EnableRotating(GameObject Model)
+    {
+        Model.GetComponent<MarkerPlace>().IsPlacementEnabled = false;
+        Model.GetComponent<HandDraggable>().IsDraggingEnabled = false;
+        Model.GetComponent<HandRotate>().IsRotatingEnabled = true;
+    }
+}
