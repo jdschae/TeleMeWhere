f740265a4eadd7527b6000bb9c7659e81cef79fd
Mitchell Trost <mrtrost@umich.edu>
2017-10-17 22:46:55 -0400

    sign out script

---
+++ Assets/Scripts/SignOut.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SignOut : MonoBehaviour {
+
+	public void SignOutAction()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+}
