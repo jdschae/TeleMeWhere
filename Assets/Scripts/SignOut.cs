a1dbcc941c3ed4535036ab2655e69a7344079028
Frank Yu <FrankYu@Fedrics-MacBook-Pro.local>
2017-10-17 23:33:44 -0400

    signout frontend script

--- Assets/Scripts/SignOut.cs
+++ Assets/Scripts/SignOut.cs
@@ -6,7 +6,11 @@ using UnityEngine.SceneManagement;
 public class SignOut : MonoBehaviour {
 
 	public void SignOutAction()
-    {
+    {	
+    	string json = "";
+    	string url = "http://35.1.109.92:3000/api/user/logout";
+        WWW www = Click_Buttons.SendPostRequest(json, url);
+        // parse databefore signout
         SceneManager.LoadScene(0);
     }
 
