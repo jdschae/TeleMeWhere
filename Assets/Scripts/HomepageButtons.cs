0a87f11493877d24491fb1e23a53626b2ba0525c
Mitchell Trost <mrtrost@umich.edu>
2017-11-05 17:00:20 -0500

    Updates 1700

--- Assets/Scripts/HomepageButtons.cs
+++ Assets/Scripts/HomepageButtons.cs
@@ -1,16 +1,75 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using UnityEngine.UI;
+using HoloToolkit.UI.Keyboard;
 
 public class HomepageButtons : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    //Homepage Canvas Screens
+    public Canvas patientHomepage;
+    public Canvas doctorHomepage;
+    public Canvas invitePatient;
+    public Canvas inviteDoc;
+    public Canvas checkInvites;
+    public Canvas editInfo;
+
+    //On start, only Homepage is visible
+    private void Awake()
+    {
+        //IF STATEMENT #FIXME
+        patientHomepage.enabled = true;
+        doctorHomepage.enabled = true;
+        invitePatient.enabled = false;
+        inviteDoc.enabled = false;
+        checkInvites.enabled = false;
+        editInfo.enabled = false;
+
+    }
+
+    //Used when clicking "Invite Patient/Doctor" from Homepage
+    public void InviteOn()
+    {
+        //IF STATEMENT #FIXME
+        patientHomepage.enabled = false;
+        doctorHomepage.enabled = false;
+        invitePatient.enabled = true;
+        inviteDoc.enabled = true;
+        checkInvites.enabled = false;
+        editInfo.enabled = false;
+    }
+
+    //Used when clicking "Check Invites" from Homepage
+    public void CheckOn()
+    {
+        patientHomepage.enabled = false;
+        doctorHomepage.enabled = false;
+        invitePatient.enabled = false;
+        inviteDoc.enabled = false;
+        checkInvites.enabled = true;
+        editInfo.enabled = false;
+    }
+
+    //Used when clicking "Edit Info" from Homepage
+    public void EditOn()
+    {
+        patientHomepage.enabled = false;
+        doctorHomepage.enabled = false;
+        invitePatient.enabled = false;
+        inviteDoc.enabled = false;
+        checkInvites.enabled = false;
+        editInfo.enabled = true;
+    }
+
+    //Used when clicking "Go Back" from Invite/Check/Edit
+    public void GoBackHome()
+    {
+        //IF STATEMENT #FIXME
+        patientHomepage.enabled = true;
+        doctorHomepage.enabled = true;
+        invitePatient.enabled = false;
+        inviteDoc.enabled = false;
+        checkInvites.enabled = false;
+        editInfo.enabled = false;
+    }
 }
