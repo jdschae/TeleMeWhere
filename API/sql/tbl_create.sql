7cccbcef8e16bf237c7519abbcf80229f86a5979
Frank Yu <FrankYu@0587377169.wireless.umich.net>
2017-11-18 23:41:34 -0500

    new api route

--- API/sql/tbl_create.sql
+++ API/sql/tbl_create.sql
@@ -3,7 +3,7 @@ USE telemewhere;
 
 CREATE TABLE User(username VARCHAR(20), sex VARCHAR(1), type VARCHAR(1), firstname VARCHAR(20), lastname VARCHAR(20), password VARCHAR(256), email VARCHAR(256), creationdate TIMESTAMP DEFAULT CURRENT_TIMESTAMP, PRIMARY KEY(username));
 CREATE TABLE Model(modelid INT NOT NULL AUTO_INCREMENT, username VARCHAR(20), creationdate TIMESTAMP DEFAULT CURRENT_TIMESTAMP, PRIMARY KEY(modelid));
-CREATE TABLE Marker(markerid INT NOT NULL AUTO_INCREMENT, modelid INT NOT NULL, x FLOAT(10,5), y FLOAT(10,5), z FLOAT(10,5), creationdate TIMESTAMP DEFAULT CURRENT_TIMESTAMP, message VARCHAR(500), PRIMARY KEY(markerid));
+CREATE TABLE Marker(markerid INT NOT NULL AUTO_INCREMENT, modelid INT NOT NULL, x FLOAT(10,5), y FLOAT(10,5), z FLOAT(10,5), creationdate TIMESTAMP DEFAULT CURRENT_TIMESTAMP, message VARCHAR(500), color VARCHAR(20), shape VARCHAR(20), rw FLOAT(10,5), rx FLOAT(10,5), ry FLOAT(10,5), rz FLOAT(10,5), PRIMARY KEY(markerid));
 CREATE TABLE Invite(username VARCHAR(20), invitee VARCHAR(20), access TINYINT(1));
 
 INSERT INTO User (username, firstname, lastname, password, email) VALUES ("jahan", "Frank", "Yu", "eecs498", "hello@gmail.com");
