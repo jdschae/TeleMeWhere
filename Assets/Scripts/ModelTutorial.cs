ec83b33030e89849400af6d183809df71066bb66
Mitchell Trost <mrtrost@umich.edu>
2017-12-10 16:33:41 -0500

    model scene tutorial

---
+++ Assets/Scripts/ModelTutorial.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class ModelTutorial : MonoBehaviour {
+
+    public Canvas selectAction;
+    public Canvas tutorial;
+
+	public void Awake() {
+        selectAction.enabled = true;
+        tutorial.enabled = false;
+	}
+
+    public void TutorialOn()
+    {
+        selectAction.enabled = false;
+        tutorial.enabled = true;
+    }
+
+    public void GoBack()
+    {
+        selectAction.enabled = true;
+        tutorial.enabled = false;
+    }
+	
+}
